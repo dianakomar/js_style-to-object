4b36ef79b22ad2603284dbe9dd768b53424f89a7
Diana Komarchuk <dianakocmarchuk2508@gmail.com>
2026-03-20 22:09:07 +0200

    version 1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,31 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const styles = sourceString
+    .split(';')
+    .map((el) => el.trim())
+    .filter(Boolean);
+
+  styles.forEach((line) => {
+    const idx = line.indexOf(':');
+
+    if (idx === -1) {
+      return;
+    }
+
+    const key = line.slice(0, idx).trim();
+    let value = line.slice(idx + 1).trim();
+
+    if (value.endsWith(';')) {
+      value = value.slice(0, -1).trim();
+    }
+
+    result[key] = value;
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;
